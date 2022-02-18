8f875fc3f87bf9ca3e8d56b01b64d33009170b1b
Queen-02 <queenzyblack176@gmail.com>
2022-02-18 19:33:57 +0300

    add changes made to main

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,17 @@
 fun main() {
     greet("Queen")
+    divide(72, 8)
 
 }
 
 //Question one
 fun greet(name: String) {
     println("Hello " + name)
+}
+
+//Question two
+fun divide(x: Int, y: Int) {
+    var div = x/y
+    println(div)
+
 }
