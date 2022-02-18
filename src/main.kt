5edf0cdca3bedce9eb626924ffe07396fffb3c26
Queen-02 <queenzyblack176@gmail.com>
2022-02-18 19:41:54 +0300

    add changed files

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,36 @@
 fun main() {
     greet("Queen")
-    divide(72, 8)
-
+    total()
 }
 
 //Question one
 fun greet(name: String) {
     println("Hello " + name)
+    divide(72, 8)
+
 }
 
 //Question two
 fun divide(x: Int, y: Int) {
     var div = x/y
     println(div)
+
+}
+
+//Question Three
+fun total() {
+    var a = 10
+    var b = 12
+    var c = 3
+    var d = 8
+    a+=b + c + d
+    println(a)
+
+    interest("and ")
+}
+
+fun interest(funny: String) {
+    var hobby = "Singing "
+    println(hobby + funny)
 
 }
